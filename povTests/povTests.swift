1d74706744891688a1086d0459904fa4616b2eff
Hazel Ruozhen Gong <ruozhen7@gmail.com>
2025-12-04 17:40:49 +0800

    Initial Commit

---
+++ povTests/povTests.swift
@@ -0,0 +1,17 @@
+//
+//  povTests.swift
+//  povTests
+//
+//  Created by Hazel Gong on 2025/12/4.
+//
+
+import Testing
+@testable import pov
+
+struct povTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ povUITests/povUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  povUITestsLaunchTests.swift
+//  povUITests
+//
+//  Created by Hazel Gong on 2025/12/4.
+//
+
+import XCTest
+
+final class povUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
